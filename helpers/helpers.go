74b769806c7df7c516af158fb4b3f1f0ca301f80
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2022-03-12 14:04:14 +0200

    adding heroku postgress credentials

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -25,7 +25,8 @@ func HashPassword(password string) string {
 
 func ConnectDB() *gorm.DB {
 	fmt.Println("Connecting to DB")
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=Twitter-Clone sslmode=disable")
+	//db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=Twitter-Clone sslmode=disable")
+	db, err := gorm.Open("postgres", "host=ec2-54-158-26-89.compute-1.amazonaws.com port=5432 user=mchmkgthfrusfo dbname=das0j74cai8cer sslmode=disable password=d8c321a53c3d4e617295b89bb6ad45a5a95ab3c8850f122f8ac71bc8e6bef1ef")
 	HandleErr(err)
 	return db
 }
