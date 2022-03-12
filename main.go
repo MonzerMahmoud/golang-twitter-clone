0a64c34fdfa76b023fdf57c42ce5c742af7450ee
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2022-03-12 14:53:13 +0200

    Fixed db env vars issue

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"golang-twitter-clone/api"
+	"golang-twitter-clone/migrations"
 )
 
 func main() {
 	//router.InitializeRouter()
 	//migrations.Migrate()
 	api.InitializeRouter()
+	migrations.Migrate()
 }
 
 
